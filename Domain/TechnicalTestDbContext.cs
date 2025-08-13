c2bd50f31a8bbb98f52f7b8549cc35bd729d11b5
EAMolina500 <molinaenzoagustin@gmail.com>
2025-08-13 14:11:25 -0300

    Configuration of Product entity

--- Domain/TechnicalTestDbContext.cs
+++ Domain/TechnicalTestDbContext.cs
@@ -10,9 +10,22 @@ namespace technical_tests_backend_ssr.Domain
 
         public DbSet<Product> Products { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+
+                entity.Property(p => p.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+                entity.Property(p => p.Price)
+                      .IsRequired()
+                      .HasColumnType("decimal(18,2)");
+            });
+        }
     }
 }
